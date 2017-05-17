5b54f41d3139a15712bb0f6107e667f71909f669
heylinart <hopezhu10@gmail.com>
2017-05-17 21:37:13 +0800

    Add files via upload

---
+++ KthLargestElement.java
@@ -0,0 +1,42 @@
+package lintcode;
+
+public class KthLargestElement {
+	/*
+     * @param k : description of k
+     * @param nums : array of nums
+     * @return: description of return
+     */
+	public static void main(String[] args){
+		int nums[]={4,5,2,4,7,8,9};
+		kthLargestElement(3,nums);
+	}
+	public static int kthLargestElement(int k, int[] nums) {
+        // write your code here
+		if(k>nums.length) return 0;
+		return helper(nums.length-k+1,nums,0,nums.length-1);
+    }
+	public static int helper(int k, int[] nums, int start, int end){
+		int left = start, right = end;
+		int pivot = end;
+		while(true){
+			while(nums[left]<nums[end] && left < right){
+				left ++;
+			}
+			while(nums[right]>=nums[pivot] && left < right){
+				right --;
+			}
+			if(left == right) break;
+			swap(nums,left,right);			
+		}
+		swap(nums,left,end);
+		if(left + 1 == k) return nums[left];
+		else if(left + 1 <k) return helper(k,nums,left+1,end);
+		else return helper(k, nums,start,left-1);
+	}
+	public static void swap(int[] nums,int left,int right){
+		int temp = nums[left];
+		nums[left] = nums[right];
+		nums[right] = temp;
+	}
+}
+
